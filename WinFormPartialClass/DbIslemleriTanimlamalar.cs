7b63b083583662e1ce3ed444704e6c8e62ee4d94
Mertcan Dursun <221903570@ogr.uludag.edu.tr>
2022-04-18 15:54:02 +0300

    last commit

---
+++ WinFormPartialClass/DbIslemleriTanimlamalar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormPartialClass
+{
+    public partial class DbIslemleri
+    {
+        public int kdvOrani = 18;
+        public string kargo = "AAA Kargo";
+
+    }
+}
